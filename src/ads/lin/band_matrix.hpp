03beb0ad97b94d6be4b2e43604d68596ea6580b3
Marcin Łoś <losiu99@gazeta.pl>
2015-07-28 11:00:53 -0500

    Add matrix classes and LAPACK solver interface

---
+++ src/ads/lin/band_matrix.hpp
@@ -0,0 +1,80 @@
+#ifndef ADS_LIN_BANDED_MATRIX_HPP_
+#define ADS_LIN_BANDED_MATRIX_HPP_
+
+#include <vector>
+#include "ads/lin/print_matrix.hpp"
+
+namespace ads {
+namespace lin {
+
+class band_matrix {
+private:
+    std::vector<double> data_;
+
+public:
+    int kl;
+    int ku;
+    int n;
+
+    band_matrix(int kl, int ku, int n)
+    : data_(array_size_(kl, ku, n))
+    , kl(kl)
+    , ku(ku)
+    , n(n)
+    { }
+
+    double& operator ()(int i, int j) {
+        int row = kl + ku + i - j;
+        int col = j;
+        return data_[col * column_size_() + row];
+    }
+
+    double operator ()(int i, int j) const {
+        if (inside_band(i, j)) {
+            auto& self = const_cast<band_matrix&>(*this);
+            return self(i, j);
+        }
+        return 0;
+    }
+
+    bool inside_band(int i, int j) const {
+        return i - j <= kl && j - i <= ku;
+    }
+
+    int cols() const {
+        return n;
+    }
+
+    int rows() const {
+        return n;
+    }
+
+    double* data() {
+        return data_.data();
+    }
+
+    const double* data() const {
+        return data_.data();
+    }
+
+private:
+
+    int column_size_() const {
+        return kl + (ku + 1 + kl);
+    }
+
+    static int array_size_(int kl, int ku, int n) {
+        int rows = kl + (ku + 1 + kl);
+        int cols = n;
+        return rows * cols;
+    }
+};
+
+inline std::ostream& operator <<(std::ostream& os, const band_matrix& m) {
+    return print_matrix(os, m);
+}
+
+}
+}
+
+#endif /* ADS_LIN_BANDED_MATRIX_HPP_ */
---
+++ src/ads/lin/band_solve.hpp
@@ -0,0 +1,58 @@
+#ifndef ADS_LIN_BAND_SOLVE_HPP_
+#define ADS_LIN_BAND_SOLVE_HPP_
+
+#include <iostream>
+#include "ads/lin/band_matrix.hpp"
+#include "ads/lin/matrix.hpp"
+
+// LAPACK routines
+extern "C" {
+
+int dgbtrf_(int* m, int* n, int* kl, int* ku, double* ab, int* ldab, int* ipiv, int* info);
+
+int dgbtrs_(const char* trans, int* n, int* kl, int* ku, int* nrhs, double* ab, int* ldab,
+        int* ipiv, double* b, int* ldb, int* info);
+}
+
+namespace ads {
+namespace lin {
+
+struct solver_ctx {
+    std::vector<int> pivot_vector;
+    int info = 0;
+    int ldab;
+
+    solver_ctx(int n, int kl, int ku)
+    : pivot_vector(n)
+    , ldab(2 * kl + ku + 1)
+    { }
+
+    solver_ctx(const band_matrix& a)
+    : solver_ctx(a.n, a.kl, a.ku)
+    { }
+
+    int* pivot() {
+        return pivot_vector.data();
+    }
+};
+
+inline void factorize(band_matrix& a, solver_ctx& ctx) {
+    dgbtrf_(&a.n, &a.n, &a.kl, &a.ku, a.data(), &ctx.ldab, ctx.pivot(), &ctx.info);
+}
+
+inline void solve_with_factorized(band_matrix& a, matrix& b, solver_ctx& ctx) {
+    int nrhs = b.cols();
+    const char* trans = "No transpose";
+
+    dgbtrs_(trans, &a.n, &a.kl, &a.ku, &nrhs, a.data(), &ctx.ldab, ctx.pivot(), b.data(), &a.n, &ctx.info);
+}
+
+inline void solve(band_matrix& a, matrix& b, solver_ctx& ctx) {
+    factorize(a, ctx);
+    solve_with_factorized(a, b, ctx);
+}
+
+}
+}
+
+#endif /* ADS_LIN_BAND_SOLVE_HPP_ */
