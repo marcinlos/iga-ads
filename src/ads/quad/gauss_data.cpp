ae5ba13eac02e7486e029d93fe32c247626fef39
Marcin Łoś <losiu99@gazeta.pl>
2015-08-04 10:19:35 -0500

    Add Gaussian quadrature

---
+++ src/ads/quad/gauss_data.cpp
@@ -0,0 +1,277 @@
+
+#include "ads/quad/gauss.hpp"
+
+namespace ads {
+namespace quad {
+namespace gauss {
+
+constexpr double gauss_data<2>::X[];
+constexpr double gauss_data<3>::X[];
+constexpr double gauss_data<4>::X[];
+constexpr double gauss_data<5>::X[];
+constexpr double gauss_data<6>::X[];
+constexpr double gauss_data<7>::X[];
+constexpr double gauss_data<8>::X[];
+constexpr double gauss_data<9>::X[];
+constexpr double gauss_data<10>::X[];
+constexpr double gauss_data<11>::X[];
+constexpr double gauss_data<12>::X[];
+constexpr double gauss_data<13>::X[];
+constexpr double gauss_data<14>::X[];
+constexpr double gauss_data<15>::X[];
+constexpr double gauss_data<16>::X[];
+constexpr double gauss_data<17>::X[];
+constexpr double gauss_data<18>::X[];
+constexpr double gauss_data<19>::X[];
+constexpr double gauss_data<20>::X[];
+constexpr double gauss_data<21>::X[];
+constexpr double gauss_data<22>::X[];
+constexpr double gauss_data<23>::X[];
+constexpr double gauss_data<24>::X[];
+constexpr double gauss_data<25>::X[];
+constexpr double gauss_data<26>::X[];
+constexpr double gauss_data<27>::X[];
+constexpr double gauss_data<28>::X[];
+constexpr double gauss_data<29>::X[];
+constexpr double gauss_data<30>::X[];
+constexpr double gauss_data<31>::X[];
+constexpr double gauss_data<32>::X[];
+constexpr double gauss_data<33>::X[];
+constexpr double gauss_data<34>::X[];
+constexpr double gauss_data<35>::X[];
+constexpr double gauss_data<36>::X[];
+constexpr double gauss_data<37>::X[];
+constexpr double gauss_data<38>::X[];
+constexpr double gauss_data<39>::X[];
+constexpr double gauss_data<40>::X[];
+constexpr double gauss_data<41>::X[];
+constexpr double gauss_data<42>::X[];
+constexpr double gauss_data<43>::X[];
+constexpr double gauss_data<44>::X[];
+constexpr double gauss_data<45>::X[];
+constexpr double gauss_data<46>::X[];
+constexpr double gauss_data<47>::X[];
+constexpr double gauss_data<48>::X[];
+constexpr double gauss_data<49>::X[];
+constexpr double gauss_data<50>::X[];
+constexpr double gauss_data<51>::X[];
+constexpr double gauss_data<52>::X[];
+constexpr double gauss_data<53>::X[];
+constexpr double gauss_data<54>::X[];
+constexpr double gauss_data<55>::X[];
+constexpr double gauss_data<56>::X[];
+constexpr double gauss_data<57>::X[];
+constexpr double gauss_data<58>::X[];
+constexpr double gauss_data<59>::X[];
+constexpr double gauss_data<60>::X[];
+constexpr double gauss_data<61>::X[];
+constexpr double gauss_data<62>::X[];
+constexpr double gauss_data<63>::X[];
+constexpr double gauss_data<64>::X[];
+
+
+constexpr double gauss_data<2>::W[];
+constexpr double gauss_data<3>::W[];
+constexpr double gauss_data<4>::W[];
+constexpr double gauss_data<5>::W[];
+constexpr double gauss_data<6>::W[];
+constexpr double gauss_data<7>::W[];
+constexpr double gauss_data<8>::W[];
+constexpr double gauss_data<9>::W[];
+constexpr double gauss_data<10>::W[];
+constexpr double gauss_data<11>::W[];
+constexpr double gauss_data<12>::W[];
+constexpr double gauss_data<13>::W[];
+constexpr double gauss_data<14>::W[];
+constexpr double gauss_data<15>::W[];
+constexpr double gauss_data<16>::W[];
+constexpr double gauss_data<17>::W[];
+constexpr double gauss_data<18>::W[];
+constexpr double gauss_data<19>::W[];
+constexpr double gauss_data<20>::W[];
+constexpr double gauss_data<21>::W[];
+constexpr double gauss_data<22>::W[];
+constexpr double gauss_data<23>::W[];
+constexpr double gauss_data<24>::W[];
+constexpr double gauss_data<25>::W[];
+constexpr double gauss_data<26>::W[];
+constexpr double gauss_data<27>::W[];
+constexpr double gauss_data<28>::W[];
+constexpr double gauss_data<29>::W[];
+constexpr double gauss_data<30>::W[];
+constexpr double gauss_data<31>::W[];
+constexpr double gauss_data<32>::W[];
+constexpr double gauss_data<33>::W[];
+constexpr double gauss_data<34>::W[];
+constexpr double gauss_data<35>::W[];
+constexpr double gauss_data<36>::W[];
+constexpr double gauss_data<37>::W[];
+constexpr double gauss_data<38>::W[];
+constexpr double gauss_data<39>::W[];
+constexpr double gauss_data<40>::W[];
+constexpr double gauss_data<41>::W[];
+constexpr double gauss_data<42>::W[];
+constexpr double gauss_data<43>::W[];
+constexpr double gauss_data<44>::W[];
+constexpr double gauss_data<45>::W[];
+constexpr double gauss_data<46>::W[];
+constexpr double gauss_data<47>::W[];
+constexpr double gauss_data<48>::W[];
+constexpr double gauss_data<49>::W[];
+constexpr double gauss_data<50>::W[];
+constexpr double gauss_data<51>::W[];
+constexpr double gauss_data<52>::W[];
+constexpr double gauss_data<53>::W[];
+constexpr double gauss_data<54>::W[];
+constexpr double gauss_data<55>::W[];
+constexpr double gauss_data<56>::W[];
+constexpr double gauss_data<57>::W[];
+constexpr double gauss_data<58>::W[];
+constexpr double gauss_data<59>::W[];
+constexpr double gauss_data<60>::W[];
+constexpr double gauss_data<61>::W[];
+constexpr double gauss_data<62>::W[];
+constexpr double gauss_data<63>::W[];
+constexpr double gauss_data<64>::W[];
+
+
+const double* Xs[] = {
+    nullptr,
+    nullptr,
+    gauss_data<2>::X,
+    gauss_data<3>::X,
+    gauss_data<4>::X,
+    gauss_data<5>::X,
+    gauss_data<6>::X,
+    gauss_data<7>::X,
+    gauss_data<8>::X,
+    gauss_data<9>::X,
+    gauss_data<10>::X,
+    gauss_data<11>::X,
+    gauss_data<12>::X,
+    gauss_data<13>::X,
+    gauss_data<14>::X,
+    gauss_data<15>::X,
+    gauss_data<16>::X,
+    gauss_data<17>::X,
+    gauss_data<18>::X,
+    gauss_data<19>::X,
+    gauss_data<20>::X,
+    gauss_data<21>::X,
+    gauss_data<22>::X,
+    gauss_data<23>::X,
+    gauss_data<24>::X,
+    gauss_data<25>::X,
+    gauss_data<26>::X,
+    gauss_data<27>::X,
+    gauss_data<28>::X,
+    gauss_data<29>::X,
+    gauss_data<30>::X,
+    gauss_data<31>::X,
+    gauss_data<32>::X,
+    gauss_data<33>::X,
+    gauss_data<34>::X,
+    gauss_data<35>::X,
+    gauss_data<36>::X,
+    gauss_data<37>::X,
+    gauss_data<38>::X,
+    gauss_data<39>::X,
+    gauss_data<40>::X,
+    gauss_data<41>::X,
+    gauss_data<42>::X,
+    gauss_data<43>::X,
+    gauss_data<44>::X,
+    gauss_data<45>::X,
+    gauss_data<46>::X,
+    gauss_data<47>::X,
+    gauss_data<48>::X,
+    gauss_data<49>::X,
+    gauss_data<50>::X,
+    gauss_data<51>::X,
+    gauss_data<52>::X,
+    gauss_data<53>::X,
+    gauss_data<54>::X,
+    gauss_data<55>::X,
+    gauss_data<56>::X,
+    gauss_data<57>::X,
+    gauss_data<58>::X,
+    gauss_data<59>::X,
+    gauss_data<60>::X,
+    gauss_data<61>::X,
+    gauss_data<62>::X,
+    gauss_data<63>::X,
+    gauss_data<64>::X,
+};
+
+
+const double* Ws[] = {
+    nullptr,
+    nullptr,
+    gauss_data<2>::W,
+    gauss_data<3>::W,
+    gauss_data<4>::W,
+    gauss_data<5>::W,
+    gauss_data<6>::W,
+    gauss_data<7>::W,
+    gauss_data<8>::W,
+    gauss_data<9>::W,
+    gauss_data<10>::W,
+    gauss_data<11>::W,
+    gauss_data<12>::W,
+    gauss_data<13>::W,
+    gauss_data<14>::W,
+    gauss_data<15>::W,
+    gauss_data<16>::W,
+    gauss_data<17>::W,
+    gauss_data<18>::W,
+    gauss_data<19>::W,
+    gauss_data<20>::W,
+    gauss_data<21>::W,
+    gauss_data<22>::W,
+    gauss_data<23>::W,
+    gauss_data<24>::W,
+    gauss_data<25>::W,
+    gauss_data<26>::W,
+    gauss_data<27>::W,
+    gauss_data<28>::W,
+    gauss_data<29>::W,
+    gauss_data<30>::W,
+    gauss_data<31>::W,
+    gauss_data<32>::W,
+    gauss_data<33>::W,
+    gauss_data<34>::W,
+    gauss_data<35>::W,
+    gauss_data<36>::W,
+    gauss_data<37>::W,
+    gauss_data<38>::W,
+    gauss_data<39>::W,
+    gauss_data<40>::W,
+    gauss_data<41>::W,
+    gauss_data<42>::W,
+    gauss_data<43>::W,
+    gauss_data<44>::W,
+    gauss_data<45>::W,
+    gauss_data<46>::W,
+    gauss_data<47>::W,
+    gauss_data<48>::W,
+    gauss_data<49>::W,
+    gauss_data<50>::W,
+    gauss_data<51>::W,
+    gauss_data<52>::W,
+    gauss_data<53>::W,
+    gauss_data<54>::W,
+    gauss_data<55>::W,
+    gauss_data<56>::W,
+    gauss_data<57>::W,
+    gauss_data<58>::W,
+    gauss_data<59>::W,
+    gauss_data<60>::W,
+    gauss_data<61>::W,
+    gauss_data<62>::W,
+    gauss_data<63>::W,
+    gauss_data<64>::W,
+};
+
+}
+}
+}
