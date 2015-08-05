c65043164208ebecc7e0b24a6c811159f9a5f5be
Marcin Łoś <losiu99@gazeta.pl>
2015-08-05 14:32:27 -0500

    Add tensor product spline evaluation (2D, 3D)

---
+++ src/ads/bspline/multidimensional.hpp
@@ -0,0 +1,77 @@
+#ifndef ADS_BSPLINE_MULTIDIMENSIONAL_HPP_
+#define ADS_BSPLINE_MULTIDIMENSIONAL_HPP_
+
+#include "ads/bspline/bspline.hpp"
+
+namespace ads {
+namespace bspline {
+
+
+template <typename U>
+double eval(double x, double y, const U& u, const basis& bx, const basis& by, eval_ctx& cx, eval_ctx& cy) {
+
+    int spanx = find_span(x, bx);
+    int spany = find_span(y, by);
+
+    double* bvx = cx.basis_vals();
+    double* bvy = cy.basis_vals();
+
+    eval_basis(spanx, x, bx, bvx, cx);
+    eval_basis(spany, y, by, bvy, cy);
+
+    int offsetx = spanx - bx.degree;
+    int offsety = spany - by.degree;
+
+    double value = 0;
+    for (int ix = 0; ix < bx.dofs_per_element(); ++ ix) {
+        for (int iy = 0; iy < by.dofs_per_element(); ++ iy) {
+            value += u(ix + offsetx, iy + offsety) * bvx[ix] * bvy[iy];
+        }
+    }
+    return value;
+}
+
+
+template <typename U>
+double eval(double x, double y, double z,
+        const U& u,
+        const basis& bx, const basis& by, const basis& bz,
+        eval_ctx& cx, eval_ctx& cy, eval_ctx& cz) {
+
+    int spanx = find_span(x, bx);
+    int spany = find_span(y, by);
+    int spanz = find_span(z, bz);
+
+    double* bvx = cx.basis_vals();
+    double* bvy = cy.basis_vals();
+    double* bvz = cz.basis_vals();
+
+    eval_basis(spanx, x, bx, bvx, cx);
+    eval_basis(spany, y, by, bvy, cy);
+    eval_basis(spanz, z, bz, bvz, cz);
+
+    int offsetx = spanx - bx.degree;
+    int offsety = spany - by.degree;
+    int offsetz = spanz - bz.degree;
+
+    double value = 0;
+    for (int ix = 0; ix < bx.dofs_per_element(); ++ ix) {
+        for (int iy = 0; iy < by.dofs_per_element(); ++ iy) {
+            for (int iz = 0; iz < bz.dofs_per_element(); ++ iz) {
+                int ixx = ix + offsetx;
+                int iyy = iy + offsety;
+                int izz = iz + offsetz;
+                value += u(ixx, iyy, izz) * bvx[ix] * bvy[iy] * bvz[iz];
+            }
+        }
+    }
+    return value;
+}
+
+
+
+}
+}
+
+
+#endif /* ADS_BSPLINE_MULTIDIMENSIONAL_HPP_ */
