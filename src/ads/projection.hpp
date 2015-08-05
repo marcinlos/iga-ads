9ac7fce35711ab3c7b4a40845b0dd87b563f8f6d
Marcin Łoś <losiu99@gazeta.pl>
2015-08-05 18:35:01 -0500

    Add 1D and 2D projection functions

--- src/ads/projection.hpp
+++ src/ads/projection.hpp
@@ -6,6 +6,62 @@
 
 namespace ads {
 
+template <typename Rhs, typename Function>
+void compute_projection(Rhs& u, const basis_data& d1, Function f) {
+    for (element_id e1 = 0; e1 < d1.elements; ++ e1) {
+        double J = d1.J[e1];
+        int first1 = d1.first_dof(e1);
+        int last1  = d1.last_dof(e1);
+
+        for (int q1 = 0; q1 < d1.quad_order; ++ q1) {
+            double w = d1.w[q1];
+            double x1 = d1.x[e1][q1];
+
+            for (int a1 = 0; a1 + first1 <= last1; ++ a1) {
+                int i1 = a1 + first1;
+
+                double B1 = d1.b[e1][q1][0][a1];
+                double B = B1;
+                u(i1) += f(x1) * B * w * J;
+            }
+        }
+    }
+}
+
+
+template <typename Rhs, typename Function>
+void compute_projection(Rhs& u, const basis_data& d1, const basis_data& d2, Function f) {
+    for (element_id e1 = 0; e1 < d1.elements; ++ e1) {
+    for (element_id e2 = 0; e2 < d2.elements; ++ e2) {
+        double J = d1.J[e1] * d2.J[e2];
+        int first1 = d1.first_dof(e1);
+        int last1  = d1.last_dof(e1);
+        int first2 = d2.first_dof(e2);
+        int last2  = d2.last_dof(e2);
+
+        for (int q1 = 0; q1 < d1.quad_order; ++ q1) {
+        for (int q2 = 0; q2 < d2.quad_order; ++ q2) {
+            double w = d1.w[q1] * d2.w[q2];
+            double x1 = d1.x[e1][q1];
+            double x2 = d2.x[e2][q2];
+
+            for (int a1 = 0; a1 + first1 <= last1; ++ a1) {
+            for (int a2 = 0; a2 + first2 <= last2; ++ a2) {
+                int i1 = a1 + first1;
+                int i2 = a2 + first2;
+
+                double B1 = d1.b[e1][q1][0][a1];
+                double B2 = d2.b[e2][q2][0][a2];
+                double B = B1 * B2;
+                u(i1, i2) += f(x1, x2) * B * w * J;
+            }
+            }
+        }
+        }
+    }
+    }
+}
+
 
 template <typename Rhs, typename Function>
 void compute_projection(Rhs& u, const basis_data& d1, const basis_data& d2, const basis_data& d3, Function f) {
