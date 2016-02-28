5de8ae20690ada07c3e9737a474320894955eb3a
Marcin Łoś <marcin.los.91@gmail.com>
2016-02-28 19:24:40 +0100

    Add example of new style simulation usage

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,28 @@
 #include <iostream>
 
-#include "ads/lin/band_matrix.hpp"
-#include "ads/basis_data.hpp"
-#include "ads/mass_matrix.hpp"
-#include "ads/lin/tensor.hpp"
-#include "ads/bspline/bspline.hpp"
-#include "ads/solver.hpp"
-#include "ads/output_manager.hpp"
-#include "ads/projection.hpp"
-#include "ads/problems/heat.hpp"
+#include "ads/util/iter/product.hpp"
+#include "ads/problems/heat/heat_1d.hpp"
+#include "ads/problems/heat/heat_3d.hpp"
+#include "ads/simulation.hpp"
 
 using namespace ads;
-
+using namespace ads::problems;
 
 
 int main() {
+//    dim_config x{ 2, 20 };
+//    timesteps_config steps{ 20000, 1e-5 };
+//    int ders = 1;
+//
+//    config_1d c{x, steps, ders};
+//    heat_1d sim{c};
+//    sim.run();
+
+    dim_config dim{ 2, 12 };
+    timesteps_config steps{ 100, 1e-7 };
+    int ders = 1;
+
+    config_3d c{dim, dim, dim, steps, ders};
+    heat_3d sim{c};
+    sim.run();
 }
