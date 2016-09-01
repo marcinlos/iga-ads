9b3e15745a8b48af133a9bacd65169f3c77dbd73
Marcin Łoś <marcin.los.91@gmail.com>
2016-09-01 13:50:25 -0500

    Implement flow problem

---
+++ src/problems/flow/geometry.hpp
@@ -0,0 +1,75 @@
+#ifndef PROBLEMS_FLOW_GEOMETRY_HPP_
+#define PROBLEMS_FLOW_GEOMETRY_HPP_
+
+#include <ostream>
+#include <cmath>
+#include "ads/util.hpp"
+
+namespace ads {
+
+struct vec3d {
+    double x;
+    double y;
+    double z;
+};
+
+inline std::ostream& operator << (std::ostream& os, const vec3d& v) {
+    return os << "(" << v.x << ", " << v.y << ", " << v.z << ")";
+}
+
+inline vec3d operator - (const vec3d& a, const vec3d& b) {
+    return {a.x - b.x, a.y - b.y, a.z - b.z};
+}
+
+inline vec3d operator + (const vec3d& b, const vec3d& a) {
+    return {a.x + b.x, a.y + b.y, a.z + b.z};
+}
+
+inline vec3d operator * (double c, const vec3d& a) {
+    return {c * a.x, c * a.y, c * a.z};
+}
+
+inline double dot(const vec3d& a, const vec3d& b) {
+    return a.x * b.x + a.y * b.y + a.z * b.z;
+}
+
+inline double len_sq(const vec3d& a) {
+    return dot(a, a);
+}
+
+inline double len(const vec3d& a) {
+    return std::sqrt(len_sq(a));
+}
+
+
+inline double falloff(double r, double R, double t) {
+    if (t < r) return 1.0;
+    if (t > R) return 0.0;
+    double h = (t - r) / (R - r);
+    return std::pow((h - 1) * (h + 1), 2);
+}
+
+// r < R in [0, 1]
+inline double bump(double r, double R, double x, double y, double z) {
+    double dx = x - 0.5;
+    double dy = y - 0.5;
+    double dz = z - 0.5;
+    double t = std::sqrt(dx * dx + dy * dy + dz * dz);
+    return falloff(r / 2, R / 2, t);
+}
+
+
+inline double dist_from_segment(const vec3d& p, const vec3d& a, const vec3d& b) {
+    auto s = b - a;
+    double proj = dot(p - a, s) / len_sq(s);
+    auto closest = lerp(clamp(proj, 0, 1), a, b);
+    return len(p - closest);
+}
+
+
+
+}
+
+
+#endif /* PROBLEMS_FLOW_GEOMETRY_HPP_
+ */
---
+++ src/problems/flow/pumps.hpp
@@ -0,0 +1,50 @@
+#ifndef ADS_PROBLEMS_FLOW_PUMPS_HPP_
+#define ADS_PROBLEMS_FLOW_PUMPS_HPP_
+
+#include <vector>
+#include "ads/util.hpp"
+#include "problems/flow/geometry.hpp"
+
+namespace ads {
+namespace problems {
+
+struct pumps {
+    std::vector<ads::vec3d> sources;
+    std::vector<ads::vec3d> sinks;
+
+    static constexpr double radius = 0.15;
+    static constexpr double pumping_strength = 1;
+    static constexpr double draining_strength = 1e5;
+
+    double pumping(double x, double y, double z) const {
+        ads::vec3d v{x, y, z};
+        double p = 0;
+        for (const auto& pos : sources) {
+            double dist = len(v - pos);
+            p += pumping_strength * ads::falloff(0, radius, dist);
+        }
+        return p;
+    }
+
+    double draining(double u, double x, double y, double z) const {
+        ads::vec3d v{x, y, z};
+        double p = 0;
+        for (const auto& pos : sinks) {
+            double dist = len(v - pos);
+            double s = draining_strength * ads::falloff(0, radius, dist);
+            p += u * s;
+        }
+        return p;
+    }
+
+    auto pumping_fun() const {
+        return [this](double x, double y, double z) {
+            return pumping(x, y, z);
+        };
+    }
+};
+
+}
+}
+
+#endif /* ADS_PROBLEMS_FLOW_PUMPS_HPP_ */
