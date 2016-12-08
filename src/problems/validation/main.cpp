c342e111f02ad19ed9f0cc86f90f7a4fccd1392e
Marcin Łoś <marcin.los.91@gmail.com>
2016-12-08 19:46:47 +0100

    Generalize validation program

--- src/problems/validation/main.cpp
+++ src/problems/validation/main.cpp
@@ -4,8 +4,17 @@ using namespace ads;
 using namespace ads::problems;
 
 
-int main() {
-    dim_config dim{ 2, 20 };
+int main(int argc, char* argv[]) {
+    if (argc < 2) {
+        std::cerr << "Usage: validation <n>" << std::endl;
+        return 0;
+    }
+    int n = std::atoi(argv[1]);
+    if (n <= 0) {
+        std::cerr << "Invalid value of n: " << argv[1] << std::endl;
+    }
+
+    dim_config dim{ 2, n };
     timesteps_config steps{ 100000, 1e-6 };
     int ders = 1;
 
