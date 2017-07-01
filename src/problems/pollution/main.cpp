b8471e314e4f914e61a0ffbbd59952a2649c8b71
Marcin Łoś <marcin.los.91@gmail.com>
2017-07-01 12:20:59 +0200

    Add 3D pollution model

--- src/problems/pollution/main.cpp
+++ src/problems/pollution/main.cpp
@@ -1,14 +1,14 @@
-#include "problems/pollution/pollution_2d.hpp"
+#include "problems/pollution/pollution_3d.hpp"
 
 using namespace ads;
 
 
 int main() {
-    dim_config dim{ 2, 400, 0, 5000 };
+    dim_config dim{ 2, 100, 0, 5000 };
     timesteps_config steps{ 10000, 10 };
     int ders = 1;
 
-    config_2d c{dim, dim, steps, ders};
-    pollution_2d sim{c};
+    config_3d c{dim, dim, dim, steps, ders};
+    pollution_3d sim{c};
     sim.run();
 }
