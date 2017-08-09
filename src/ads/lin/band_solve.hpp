659fc8efdbcd27e9e27d7aa43f509ce76ab2fc56
Marcin Łoś <marcin.los.91@gmail.com>
2017-08-09 11:27:00 -0500

    Extract solver_ctx to a separate file

--- src/ads/lin/band_solve.hpp
+++ src/ads/lin/band_solve.hpp
@@ -4,32 +4,15 @@
 #include <iostream>
 #include "ads/lin/band_matrix.hpp"
 #include "ads/lin/tensor.hpp"
+#include "ads/lin/solver_ctx.hpp"
 #include "ads/lin/lapack.hpp"
 
 namespace ads {
 namespace lin {
 
-struct solver_ctx {
-    std::vector<int> pivot_vector;
-    int info = 0;
-    int ldab;
-
-    solver_ctx(int n, int kl, int ku)
-    : pivot_vector(n)
-    , ldab(2 * kl + ku + 1)
-    { }
-
-    solver_ctx(const band_matrix& a)
-    : solver_ctx(a.rows, a.kl, a.ku)
-    { }
-
-    int* pivot() {
-        return pivot_vector.data();
-    }
-};
 
 inline void factorize(band_matrix& a, solver_ctx& ctx) {
-    dgbtrf_(&a.rows, &a.cols, &a.kl, &a.ku, a.full_buffer(), &ctx.ldab, ctx.pivot(), &ctx.info);
+    dgbtrf_(&a.rows, &a.cols, &a.kl, &a.ku, a.full_buffer(), &ctx.lda, ctx.pivot(), &ctx.info);
 }
 
 template <typename Rhs>
@@ -37,7 +20,7 @@ inline void solve_with_factorized(const band_matrix& a, Rhs& b, solver_ctx& ctx)
     int nrhs = b.size() / b.size(0);
     const char* trans = "No transpose";
 
-    dgbtrs_(trans, &a.cols, &a.kl, &a.ku, &nrhs, a.full_buffer(), &ctx.ldab, ctx.pivot(), b.data(), &a.cols, &ctx.info);
+    dgbtrs_(trans, &a.cols, &a.kl, &a.ku, &nrhs, a.full_buffer(), &ctx.lda, ctx.pivot(), b.data(), &a.cols, &ctx.info);
 }
 
 template <typename Rhs>
