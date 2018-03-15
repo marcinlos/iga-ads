9ce175416dc888927315d5a7404da7aa13028a0c
Marcin Łoś <marcin.los.91@gmail.com>
2018-03-15 02:25:28 +0100

    Change Shishkin mesh generation

--- src/problems/erikkson/main.cpp
+++ src/problems/erikkson/main.cpp
@@ -14,7 +14,7 @@ bspline::basis create_basis(double a, double b, int p, int elements, int repeate
     }
 
     auto x0 = 0.5;
-    auto y0 = 0.99;
+    auto y0 = 1 - 1e-5;
 
     for (int i = 1; i < points - 1; ++i) {
         auto t = lerp(i, elements, 0.0, 1.0);
@@ -24,6 +24,7 @@ bspline::basis create_basis(double a, double b, int p, int elements, int repeate
             knot[p + 1 + (i - 1) * r + j] = lerp(s, a, b);
         }
     }
+
     // auto x0 = 1.0/3;
     // auto x1 = 2.0/3;
 
@@ -40,6 +41,40 @@ bspline::basis create_basis(double a, double b, int p, int elements, int repeate
     //         knot[p + 1 + (i - 1) * r + j] = lerp(s, a, b);
     //     }
     // }
+    // for (auto x : knot) {
+    //     std::cout << std::setprecision(20) << x << std::endl;
+    // }
+    return {std::move(knot), p};
+}
+
+bspline::basis create_adapted_basis(double a, double b, int p, int repeated_nodes) {
+    double eps = 1e-6;
+    double dx = 0.5;
+    int points = 3;
+    while (dx > eps) {
+        dx /= 2;
+        ++ points;
+    }
+    int r = repeated_nodes + 1;
+    int knot_size = 2 * (p + 1) + (points - 2) * r;
+    bspline::knot_vector knot(knot_size);
+
+    for (int i = 0; i <= p; ++i) {
+        knot[i] = a;
+        knot[knot_size - i - 1] = b;
+    }
+
+    dx = 0.5;
+    double t = dx;
+    for (int i = 1; i < points - 1; ++i) {
+        // auto t = lerp(i, elements, 0.0, 1.0);
+
+        for (int j = 0; j < r; ++ j) {
+            knot[p + 1 + (i - 1) * r + j] = lerp(t, a, b);
+        }
+        dx /= 2;
+        t += dx;
+    }
     return {std::move(knot), p};
 }
 
@@ -87,16 +122,20 @@ int main(int argc, char* argv[]) {
     std::cout << "adaptations: " << std::boolalpha << adapt << std::endl;
 
 
-    timesteps_config steps{ nsteps, 0.5*1e-2 };
+    timesteps_config steps{ nsteps, 1e-5 };
     int ders = 1;
 
     auto trial_basis_x = create_basis(0, 1, p_trial, n, p_trial - 1 - C_trial, adapt);
+    // auto trial_basis_x = create_adapted_basis(0, 1, p_trial, p_trial - 1 - C_trial);
+
     auto dtrial_x = dimension{ trial_basis_x, quad, ders };
 
     auto trial_basis_y = bspline::create_basis(0, 1, p_trial, n, p_trial - 1 - C_trial);
     auto dtrial_y = dimension{ trial_basis_y, quad, ders };
 
     auto test_basis_x = create_basis(0, 1, p_test, n, p_test - 1 - C_test, adapt);
+    // auto test_basis_x = create_adapted_basis(0, 1, p_test, p_test - 1 - C_test);
+
     auto dtest_x = dimension{ test_basis_x, quad, ders };
 
     auto test_basis_y = bspline::create_basis(0, 1, p_test, n, p_test - 1 - C_test);
