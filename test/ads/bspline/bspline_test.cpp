e77d1587f0789e8d3234c3a25ecffa25b380e5c9
Marcin Łoś <marcin.los.91@gmail.com>
2020-11-20 02:50:17 +0100

    Add tests for span finding with repeated nodes

--- test/ads/bspline/bspline_test.cpp
+++ test/ads/bspline/bspline_test.cpp
@@ -35,6 +35,32 @@ TEST_CASE("B-spline basis", "[splines]") {
         }
     }
 
+    SECTION("Finding span with repeated nodes") {
+        basis b_rep({0, 0, 0, 0, 1, 1, 2, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5}, 3);
+
+        SECTION("point outside domain") {
+            CHECK(find_span(-4, b_rep) == 3);
+            CHECK(find_span(8, b_rep) == 13);
+        }
+        SECTION("point on domain boundary") {
+            CHECK(find_span(0, b_rep) == 3);
+            CHECK(find_span(5, b_rep) == 13);
+        }
+        SECTION("point on element boundary") {
+            CHECK(find_span(1, b_rep) == 5);
+            CHECK(find_span(2, b_rep) == 6);
+            CHECK(find_span(3, b_rep) == 9);
+            CHECK(find_span(4, b_rep) == 13);
+        }
+        SECTION("point in element interior") {
+            CHECK(find_span(0.3, b_rep) == 3);
+            CHECK(find_span(1.2, b_rep) == 5);
+            CHECK(find_span(2.8, b_rep) == 6);
+            CHECK(find_span(3.1, b_rep) == 9);
+            CHECK(find_span(4.9, b_rep) == 13);
+        }
+    }
+
     SECTION("First non-zero dofs") {
         REQUIRE_THAT(first_nonzero_dofs(b), Equals<int>({0, 1, 2, 3}));
     }
