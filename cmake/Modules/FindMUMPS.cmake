71cd1e18d7ad3a4a17d1ca474f2585400e11ca27
Marcin Łoś <marcin.los.91@gmail.com>
2021-06-19 20:51:44 +0200

    Improve FindMUMPS
    Instead of linking gfortran directly, append contents of variable
    CMAKE_Fortran_IMPLICIT_LINK_LIBRARIES to the list of MUMPS libraries.
    Also, remove some unnecessary PATH_SUFFIXES from find_ calls.

--- cmake/Modules/FindMUMPS.cmake
+++ cmake/Modules/FindMUMPS.cmake
@@ -1,13 +1,11 @@
 include(FindPackageHandleStandardArgs)
 
-
-find_path(MUMPS_INCLUDE_DIR
-  NAMES dmumps_c.h
-  PATH_SUFFIXES include mumps)
+# Find the C dobule header file
+find_path(MUMPS_INCLUDE_DIR NAMES dmumps_c.h)
 
 find_library(MUMPS_LIBRARY
   NAMES dmumps dmumps_seq
-  PATH_SUFFIXES lib lib64)
+)
 
 find_package_handle_standard_args(MUMPS
   FOUND_VAR MUMPS_FOUND
@@ -18,18 +16,15 @@ find_package_handle_standard_args(MUMPS
 
 set(MUMPS_LIBRARIES ${MUMPS_LIBRARY})
 
-find_library(MUMPS_COMMON_LIBRARY
-  NAMES mumps_common
-  PATH_SUFFIXES lib lib64)
+# mumps_common may be a separate library
+find_library(MUMPS_COMMON_LIBRARY NAMES mumps_common)
 
 if (MUMPS_COMMON_LIBRARY)
   list(APPEND MUMPS_LIBRARIES ${MUMPS_COMMON_LIBRARY})
 endif()
 
 # Find scalapack
-find_library(MUMPS_SCALAPACK
-  NAMES scalapack
-  PATH_SUFFIXES lib lib64)
+find_library(MUMPS_SCALAPACK NAMES scalapack)
 
 if (MUMPS_SCALAPACK)
   list(APPEND MUMPS_LIBRARIES ${MUMPS_SCALAPACK})
@@ -39,26 +34,25 @@ endif()
 set(ORDERING_LIBS metis parmetis ptesmumps scotch ptscotch ptscotcherr pord)
 
 foreach (lib IN LISTS ORDERING_LIBS)
-  find_library(MUMPS_${lib}_LIBRARY
-    NAMES ${lib}
-    PATH_SUFFIXES lib lib64)
+  find_library(MUMPS_${lib}_LIBRARY NAMES ${lib})
 
   if (MUMPS_${lib}_LIBRARY)
     list(APPEND MUMPS_LIBRARIES ${MUMPS_${lib}_LIBRARY})
   endif()
 endforeach()
 
-
-list(APPEND MUMPS_LIBRARIES gfortran)
-
+# We need to find some Fortran components
 enable_language(Fortran)
 
+# Link with Fortran standard library
+list(APPEND MUMPS_LIBRARIES ${CMAKE_Fortran_IMPLICIT_LINK_LIBRARIES})
+
 # Find MPI
-find_package(MPI REQUIRED)
+find_package(MPI REQUIRED COMPONENTS CXX Fortran)
 list(APPEND MUMPS_LIBRARIES MPI::MPI_CXX MPI::MPI_Fortran)
 
 # Try to enable OMP support to leverage MUMPS parallelism
-find_package(OpenMP)
+find_package(OpenMP COMPONENTS Fortran)
 
 if (OpenMP_FOUND)
   list(APPEND MUMPS_LIBRARIES OpenMP::OpenMP_Fortran)
@@ -66,9 +60,8 @@ endif()
 
 # Create imported targets
 if (MUMPS_FOUND AND NOT TARGET MUMPS::MUMPS)
-  add_library(MUMPS::MUMPS UNKNOWN IMPORTED)
+  add_library(MUMPS::MUMPS INTERFACE IMPORTED)
   set_target_properties(MUMPS::MUMPS PROPERTIES
-    IMPORTED_LOCATION "${MUMPS_LIBRARY}"
     INTERFACE_INCLUDE_DIRECTORIES "${MUMPS_INCLUDE_DIR}"
     INTERFACE_LINK_LIBRARIES "${MUMPS_LIBRARIES}")
 endif()
