1e6b23e1c03c1e47ff5c4aa92dc240b20731ccc8
Marcin Łoś <marcin.los.91@gmail.com>
2021-07-05 18:07:08 +0200

    Make git commit SHA information optional
    If git executable is not found, commit SHA is empty in the version_info
    structure. If git rev-parse fails, it is assumed the source was obtained
    by downloading a release instead of cloning the git repo, so it is not
    treated as an error.
    
    Closes #22

--- cmake/Version.cmake
+++ cmake/Version.cmake
@@ -1,15 +1,21 @@
-find_package(Git REQUIRED)
+find_package(Git)
 
-# Read last commit SHA
-execute_process(
-  COMMAND ${GIT_EXECUTABLE} rev-parse HEAD
-  RESULT_VARIABLE result
-  OUTPUT_VARIABLE ADS_GIT_COMMIT
-  OUTPUT_STRIP_TRAILING_WHITESPACE
-)
+if (NOT Git_FOUND)
+  message("Git not available, no commit SHA")
+else()
+  # Read last commit SHA
+  execute_process(
+    COMMAND ${GIT_EXECUTABLE} rev-parse HEAD
+    RESULT_VARIABLE result
+    OUTPUT_VARIABLE ADS_GIT_COMMIT
+    OUTPUT_STRIP_TRAILING_WHITESPACE
+  )
 
-if (result)
-  message(FATAL_ERROR "Failed to get git commit SHA: ${result}")
+  if (result)
+    # If the command failed, the most likely reason is that
+    # the project is not in Git repository (e.g. tarball release)
+    message("Failed to get git commit SHA: ${result}")
+  endif()
 endif()
 
 configure_file(src/ads/version.cpp.in ads/version.cpp @ONLY)
