46bdb8139329788e4a805e10566798a915abe5be
Marcin Łoś <marcin.los.91@gmail.com>
2021-07-07 23:59:30 +0200

    Add a check if git repo is the correct one
    If some other project includes IGA-ADS as a subdirectory in such a way
    that it is no longer a git repository, git commands will pick up the
    outer project repository. Add a check to prevent that.
    
    Closes #37

--- cmake/Version.cmake
+++ cmake/Version.cmake
@@ -4,33 +4,48 @@ set(ADS_VERSION_FULL "${ADS_VERSION}")
 find_package(Git)
 
 if (Git_FOUND)
-  # Read last commit SHA
+  # Determine if we are in the (correct) Git repository
   execute_process(
-    COMMAND ${GIT_EXECUTABLE} rev-parse HEAD
+    COMMAND ${GIT_EXECUTABLE} rev-parse --show-toplevel
     WORKING_DIRECTORY "${PROJECT_SOURCE_DIR}"
     RESULT_VARIABLE exit_code
-    OUTPUT_VARIABLE ADS_GIT_COMMIT
+    OUTPUT_VARIABLE ADS_GIT_WORKTREE_ROOT
     OUTPUT_STRIP_TRAILING_WHITESPACE
     ERROR_QUIET
   )
 
   if (exit_code)
-    # If the command failed, the most likely reason is that
-    # the project is not in Git repository (e.g. tarball release)
-    message(STATUS "Failed to get git commit SHA: ${exit_code}")
+    message(STATUS "No git repository found")
+  elseif(NOT ADS_GIT_WORKTREE_ROOT STREQUAL PROJECT_SOURCE_DIR)
+    message(STATUS "ADS git repository unavailable")
   else()
-    # Is HEAD exactly on a version tag?
+    # Read last commit SHA
     execute_process(
-      COMMAND ${GIT_EXECUTABLE} describe --exact-match --match "v[0-9]*" HEAD
-      RESULT_VARIABLE inexact_version
-      OUTPUT_QUIET ERROR_QUIET
+      COMMAND ${GIT_EXECUTABLE} rev-parse HEAD
+      WORKING_DIRECTORY "${PROJECT_SOURCE_DIR}"
+      RESULT_VARIABLE exit_code
+      OUTPUT_VARIABLE ADS_GIT_COMMIT
+      OUTPUT_STRIP_TRAILING_WHITESPACE
+      ERROR_QUIET
     )
 
-    # If not, append GIT commit SHA to full version string
-    if (inexact_version)
-      set(ADS_VERSION_FULL "${ADS_VERSION}+${ADS_GIT_COMMIT}")
-    endif()
+    if (exit_code)
+      # If the command failed, the most likely reason is that
+      # the project is not in Git repository (e.g. tarball release)
+      message(STATUS "Failed to get git commit SHA: ${exit_code}")
+    else()
+      # Is HEAD exactly on a version tag?
+      execute_process(
+        COMMAND ${GIT_EXECUTABLE} describe --exact-match --match "v[0-9]*" HEAD
+        RESULT_VARIABLE inexact_version
+        OUTPUT_QUIET ERROR_QUIET
+      )
 
+      # If not, append GIT commit SHA to full version string
+      if (inexact_version)
+        set(ADS_VERSION_FULL "${ADS_VERSION}+${ADS_GIT_COMMIT}")
+      endif()
+    endif()
   endif()
 else()
   message(STATUS "Git not available, no commit SHA")
