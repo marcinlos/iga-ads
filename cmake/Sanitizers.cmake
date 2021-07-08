4297713e886bef1df976235279ad31e3221e0dc6
Marcin Łoś <marcin.los.91@gmail.com>
2021-07-08 03:18:21 +0200

    Add flags for controlling sanitizers
    Closes #10

--- cmake/Sanitizers.cmake
+++ cmake/Sanitizers.cmake
@@ -1,9 +1,21 @@
-# Enable address sanitizer
-target_compile_options(project-options
-  INTERFACE
-  $<$<CONFIG:Debug>:-fsanitize=address,undefined;-fno-omit-frame-pointer>)
+if (NOT ADS_USE_SANITIZERS)
+  return()
+endif()
 
-target_link_options(project-options
-  INTERFACE
-  $<$<CONFIG:Debug>:-fsanitize=address,undefined>)
+list(JOIN ADS_USE_SANITIZERS "," SANITIZER_LIST)
+set(SANITIZER_FLAG "-fsanitize=${SANITIZER_LIST}")
 
+target_compile_options(project-options INTERFACE ${SANITIZER_FLAG})
+target_link_options(project-options INTERFACE ${SANITIZER_FLAG})
+
+target_compile_options(project-options INTERFACE "-fno-omit-frame-pointer")
+
+# Build types are case-insensitive
+string(TOUPPER "${CMAKE_BUILD_TYPE}" UPPER_BUILD_TYPE)
+
+# Enable symbol infor for build types that usually omit it
+# Without it stack traces have no line numbers
+if (NOT UPPER_BUILD_TYPE STREQUAL "DEBUG" AND
+    NOT UPPER_BUILD_TYPE STREQUAL "RELWITHDEBINFO")
+  target_compile_options(project-options INTERFACE -g)
+endif()
