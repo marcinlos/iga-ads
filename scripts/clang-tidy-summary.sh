0ce7e51f14643f7223fa00ce6c47bdd8f2439477
Marcin Łoś <marcin.los.91@gmail.com>
2021-07-10 22:51:58 +0200

    Update clang-tidy output parser for version 12
    New output is colored (ANSI escape codes) and bundles alias warnings
    together as [warning1,warning2,...,warningN]

--- scripts/clang-tidy-summary.sh
+++ scripts/clang-tidy-summary.sh
@@ -7,9 +7,11 @@ file=${1:-warnings}
 root=$( git rev-parse --show-toplevel )
 
 script="
+    s~\x1B\[[0-9;]*[a-zA-Z]~~g
     \~^${root}~!d
-    s/.\+\[\([a-zA-Z.-]\+\)\]/\1/
+    s/.\+\[\([a-zA-Z,.-]\+\)\]/\1/
     \~^${root}~d
+    s/,/\n/
 "
 
 sed "${script}" "${file}" | sort | uniq --count | sort -n --reverse
