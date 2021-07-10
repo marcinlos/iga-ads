marcinlos/iga-ads

commit 2e54493bd800115c9951469caea2255039df1156

Clean up the build system files

cmake/Setup.cmake
@@ -1,0 +1,24 @@
+# We use C++17
+set(CMAKE_CXX_STANDARD 17)
+set(CMAKE_CXX_STANDARD_REQUIRED ON)
+set(CMAKE_CXX_EXTENSIONS OFF)
+
+list(APPEND CMAKE_MODULE_PATH "${PROJECT_SOURCE_DIR}/cmake/Modules/")
+
+# Default to Release build
+if (NOT CMAKE_BUILD_TYPE AND NOT GENERATOR_IS_MULTI_CONFIG)
+  message(STATUS "No build type selected, default to Release")
+  set(CMAKE_BUILD_TYPE "Release")
+endif()
+
+# Detect whether ADS is build as top-level project
+if (CMAKE_PROJECT_NAME STREQUAL PROJECT_NAME)
+  set(ADS_IS_TOP_LEVEL TRUE)
+else()
+  set(ADS_IS_TOP_LEVEL FALSE)
+endif()
+
+# Imaginary libraries to propagate settings
+add_library(ads-options-public INTERFACE)
+add_library(ads-options-private INTERFACE)
+target_compile_features(ads-options-public INTERFACE cxx_std_17)

tests/CMakeLists.txt
@@ -1,3 +1,12 @@
+# Ensure BUILD_TESTING is defined
+include(CTest)
+
+# Only build tests if:
+# a) we are top-level project and BUILD_TESTING is TRUE
+# b) both BUILD_TESTING and ADS_BUILD_TESTING are TRUE
+if (NOT (BUILD_TESTING AND (ADS_IS_TOP_LEVEL OR ADS_BUILD_TESTING)))
+  return()
+endif()
 
 find_package(Catch2 REQUIRED)
 include(Catch)

CMakeLists.txt
@@ -5,20 +5,13 @@ project(ADS
   LANGUAGES CXX
 )
 
-# We use C++17
-set(CMAKE_CXX_STANDARD 17)
-set(CMAKE_CXX_STANDARD_REQUIRED ON)
-set(CMAKE_CXX_EXTENSIONS OFF)
+# General project-wide settings
+include(cmake/Setup.cmake)
 
-list(APPEND CMAKE_MODULE_PATH "${PROJECT_SOURCE_DIR}/cmake/Modules/")
+# --------------------------------------------------------------------
+# Build options
+# --------------------------------------------------------------------
 
-# Default to Release build
-if (NOT CMAKE_BUILD_TYPE AND NOT GENERATOR_IS_MULTI_CONFIG)
-  message(STATUS "No build type selected, default to Release")
-  set(CMAKE_BUILD_TYPE "Release")
-endif()
-
-# Options
 option(ADS_USE_MUMPS "Use MUMPS solver" OFF)
 option(ADS_USE_GALOIS "Use Galois framework" OFF)
 option(ADS_BUILD_PROBLEMS "Build example problems" ON)
@@ -26,13 +19,9 @@ option(ADS_BUILD_TOOLS "Build supporting applications" ON)
 option(ADS_BUILD_TESTING "Build tests (even as a subproject)" OFF)
 set(ADS_USE_SANITIZERS "" CACHE STRING "List of sanitizers to use (semicolon separated)")
 
-include(CTest)
-include(GNUInstallDirs)
-
-# Imaginary libraries to propagate settings
-add_library(ads-options-public INTERFACE)
-add_library(ads-options-private INTERFACE)
-target_compile_features(ads-options-public INTERFACE cxx_std_17)
+# --------------------------------------------------------------------
+# Other settings
+# --------------------------------------------------------------------
 
 # Compiler warning options
 include(cmake/Warnings.cmake)
@@ -43,25 +32,15 @@ include(cmake/Sanitizers.cmake)
 # Version information
 include(cmake/Version.cmake)
 
+# --------------------------------------------------------------------
 # Target library definition
+# --------------------------------------------------------------------
+
 add_library(ADS)
 add_library(ADS::ADS ALIAS ADS)
 set_target_properties(ADS PROPERTIES OUTPUT_NAME ads)
 
-target_sources(ADS
-  PRIVATE
-    "${CMAKE_CURRENT_BINARY_DIR}/ads/version.cpp"
-    src/ads/form_matrix.cpp
-    src/ads/bspline/bspline.cpp
-    src/ads/quad/gauss_data.cpp
-    src/ads/basis_data.cpp
-    src/ads/simulation/dimension.cpp
-    src/ads/simulation/simulation_base.cpp
-    src/ads/simulation/simulation_1d.cpp
-    src/ads/simulation/simulation_2d.cpp
-    src/ads/simulation/simulation_3d.cpp
-)
-
+# Apply project settings
 target_link_libraries(ADS PUBLIC ads-options-public)
 target_link_libraries(ADS PRIVATE ads-options-private)
 
@@ -71,9 +50,10 @@ target_include_directories(ADS
 )
 
 # --------------------------------------------------------------------
-# Libraries
+# Dependencies
 # --------------------------------------------------------------------
 
+# Bundled with the project
 add_subdirectory(external)
 
 # Numerical libraries
@@ -81,51 +61,34 @@ find_package(BLAS REQUIRED)
 find_package(LAPACK REQUIRED)
 target_link_libraries(ADS PUBLIC ${LAPACK_LIBRARIES} ${BLAS_LIBRARIES})
 
-# Boost
 find_package(Boost 1.58.0 REQUIRED)
 target_link_libraries(ADS PUBLIC Boost::boost)
 
-# fmt
 find_package(fmt 7.1 REQUIRED)
 target_link_libraries(ADS PUBLIC fmt::fmt)
 
-# MUMPS
 if (ADS_USE_MUMPS)
   find_package(MUMPS REQUIRED)
   target_link_libraries(ADS PUBLIC MUMPS::MUMPS)
 endif()
 
-# Galois
 if (ADS_USE_GALOIS)
   find_package(Galois 6.0 REQUIRED)
   target_link_libraries(ADS PUBLIC Galois::shmem)
   target_sources(ADS PRIVATE src/ads/executor/galois.cpp)
 endif()
 
-# Helper for defining programs using ads
-include(cmake/AddProgram.cmake)
-
 # --------------------------------------------------------------------
-# Other targets
+# Subdirectories
 # --------------------------------------------------------------------
 
-# Tests
-if ((CMAKE_PROJECT_NAME STREQUAL PROJECT_NAME OR ADS_BUILD_TESTING) AND BUILD_TESTING)
-  add_subdirectory(tests)
-endif()
-
-# Example problems
-if (ADS_BUILD_PROBLEMS)
-  add_subdirectory(examples)
-endif()
-
-# Tools
-if (ADS_BUILD_TOOLS)
-  add_subdirectory(tools)
-endif()
+add_subdirectory(src)
+add_subdirectory(tests)
+add_subdirectory(examples)
+add_subdirectory(tools)
 
 # --------------------------------------------------------------------
-# Installation
+# Installation & distribution
 # --------------------------------------------------------------------
 
 include(cmake/Installation.cmake)

examples/CMakeLists.txt
@@ -1,3 +1,10 @@
+if (NOT ADS_BUILD_PROBLEMS)
+  return()
+endif()
+
+include(AddProgram)
+
+# Helper function prefixing target names with "ads-example-"
 function(add_example name)
   set(TARGET_NAME "ads-example-${name}")
   add_program(${TARGET_NAME} ${ARGN})
@@ -6,6 +13,10 @@ function(add_example name)
     set_target_properties(${TARGET_NAME} PROPERTIES OUTPUT_NAME ${name})
   endif()
 endfunction()
+
+# --------------------------------------------------------------------
+# Problem definitions
+# --------------------------------------------------------------------
 
 add_example(heat_1d
   SRC

src/CMakeLists.txt
@@ -1,0 +1,15 @@
+target_sources(ADS
+  PRIVATE
+    ads/basis_data.cpp
+    ads/form_matrix.cpp
+    ads/bspline/bspline.cpp
+    ads/quad/gauss_data.cpp
+    ads/simulation/dimension.cpp
+    ads/simulation/simulation_base.cpp
+    ads/simulation/simulation_1d.cpp
+    ads/simulation/simulation_2d.cpp
+    ads/simulation/simulation_3d.cpp
+)
+
+# Add configured source file with version information
+target_sources(ADS PRIVATE "${CMAKE_CURRENT_BINARY_DIR}/ads/version.cpp")

tools/CMakeLists.txt
@@ -1,3 +1,10 @@
+if (NOT ADS_BUILD_TOOLS)
+  return()
+endif()
+
+include(AddProgram)
+
+# Helper function prefixing target and executable names with "ads-"
 function(add_tool name)
   set(TARGET_NAME "ads-${name}")
 
@@ -8,5 +15,8 @@ function(add_tool name)
   endif()
 endfunction()
 
-add_tool(error GALOIS SRC error.cpp)
+# --------------------------------------------------------------------
+# Tool definitions
+# --------------------------------------------------------------------
 
+add_tool(error GALOIS SRC error.cpp)

cmake/Installation.cmake
@@ -1,3 +1,4 @@
+include(GNUInstallDirs)
 
 # Define an export set for the main library
 install(
@@ -43,6 +44,7 @@ install(
   DIRECTORY cmake/Modules
   DESTINATION "${CMAKE_INSTALL_LIBDIR}/cmake/ads"
   COMPONENT ads-devel
+  FILES_MATCHING PATTERN "Find*"
 )
 
 # Copy public header files

commit 3413349e5b8a5f13f7bc89322cc2b7f98508265d

Move include(CTest) to top level file
This is necessary to ensure ctest can be called from the root of the
build directory. This was accidentally broken in 2e54493bd800115c
during refactoring the build files.

cmake/Setup.cmake
@@ -22,3 +22,6 @@ endif()
 add_library(ads-options-public INTERFACE)
 add_library(ads-options-private INTERFACE)
 target_compile_features(ads-options-public INTERFACE cxx_std_17)
+
+# Ensure BUILD_TESTING is defined
+include(CTest)

tests/CMakeLists.txt
@@ -1,6 +1,3 @@
-# Ensure BUILD_TESTING is defined
-include(CTest)
-
 # Only build tests if:
 # a) we are top-level project and BUILD_TESTING is TRUE
 # b) both BUILD_TESTING and ADS_BUILD_TESTING are TRUE