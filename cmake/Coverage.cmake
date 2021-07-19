86b558f2600bfaa075ec2156e96a68a7c95cc252
Marcin Łoś <marcin.los.91@gmail.com>
2021-07-19 20:04:26 +0200

    Move coverage support to CMake configuration
    This makes it possible to run it locally.

---
+++ cmake/Coverage.cmake
@@ -0,0 +1,50 @@
+if (NOT ADS_ENABLE_COVERAGE)
+  return()
+endif()
+
+find_program(ADS_LLVM_COV llvm-cov)
+find_program(ADS_LLVM_PROFDATA llvm-profdata)
+
+if (CMAKE_CXX_COMPILER_ID STREQUAL "GNU")
+
+  set(ADS_CXX_FLAGS_COVERAGE --coverage -fno-inline -O0)
+  set(ADS_EXE_LINKER_FLAGS_COVERAGE --coverage)
+
+elseif (CMAKE_CXX_COMPILER_ID MATCHES "Clang")
+
+  set(ADS_CXX_FLAGS_COVERAGE
+    -fprofile-instr-generate
+    -fcoverage-mapping
+    -O0
+    -fno-inline
+    -fno-elide-constructors
+  )
+
+  set(ADS_EXE_LINKER_FLAGS_COVERAGE
+    -fprofile-instr-generate
+  )
+
+  add_custom_target(coverage
+    COMMAND
+      ${CMAKE_COMMAND} -E env
+      LLVM_PROFILE_FILE="ads-%p.profraw" ${CMAKE_CTEST_COMMAND}
+    COMMAND
+      find ${PROJECT_BINARY_DIR} -name "ads-*.profraw" > profraw-files
+    COMMAND
+      ${ADS_LLVM_PROFDATA}  merge -sparse
+        -input-files profraw-files
+        -o coverage.profdata
+    COMMAND
+      ${ADS_LLVM_COV} show
+        -instr-profile coverage.profdata
+        $<TARGET_FILE:ads-suite>
+        ${PROJECT_SOURCE_DIR}/include
+        ${PROJECT_SOURCE_DIR}/src
+        > coverage.txt
+    DEPENDS ads-suite
+  )
+
+endif()
+
+target_compile_options(ads-options-private INTERFACE ${ADS_CXX_FLAGS_COVERAGE})
+target_link_options(ads-options-private INTERFACE ${ADS_EXE_LINKER_FLAGS_COVERAGE})
