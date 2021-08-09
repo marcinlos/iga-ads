08df3801236015860b5c920d1421eac070056b4a
Marcin Łoś <marcin.los.91@gmail.com>
2021-08-09 22:18:03 +0200

    Add structs representing Maxwell norms and errors

---
+++ examples/maxwell/results.cpp
@@ -0,0 +1,23 @@
+// SPDX-FileCopyrightText: 2015 - 2021 Marcin Łoś <marcin.los.91@gmail.com>
+// SPDX-License-Identifier: MIT
+
+#include "results.hpp"
+
+#include <iostream>
+
+auto print_result_info(maxwell_result_info const& res) -> void {
+    std::cout << "  |E|     = " << res.E.norm.total().L2 << "  " << res.E.norm.total().H1 << '\n'
+              << "  |rot E| = " << res.E.norm.rot << '\n'
+              << "  |div E| = " << res.E.norm.div << '\n'
+              << "  E err L2 = " << res.E.error.total().L2 << "  " << res.E.error.total().H1 << '\n'
+              << "    rel L2 = " << res.E.relative_error().L2 * 100 << "%  "
+              << res.E.relative_error().H1 * 100 << "%" << '\n'
+              << "  E err rot = " << res.E.error.rot << '\n'
+              << "  |H| = " << res.H.norm.total().L2 << "  " << res.H.norm.total().H1 << '\n'
+              << "  |rot H| = " << res.H.norm.rot << '\n'
+              << "  |div H| = " << res.H.norm.div << '\n'
+              << "  H err L2 = " << res.H.error.total().L2 << "  " << res.H.error.total().H1 << '\n'
+              << "    rel L2 = " << res.H.relative_error().L2 * 100 << "%  "
+              << res.H.relative_error().H1 * 100 << "%" << '\n'
+              << "  H err rot = " << res.H.error.rot << std::endl;
+}
---
+++ examples/maxwell/results.hpp
@@ -0,0 +1,48 @@
+// SPDX-FileCopyrightText: 2015 - 2021 Marcin Łoś <marcin.los.91@gmail.com>
+// SPDX-License-Identifier: MIT
+
+#ifndef MAXWELL_RESULTS_HPP
+#define MAXWELL_RESULTS_HPP
+
+#include <cmath>
+
+struct scalar_norm {
+    double L2;
+    double H1;
+};
+
+struct vector_norm {
+    scalar_norm x, y, z;
+    double rot;
+    double div;
+
+    auto total() const -> scalar_norm {
+        return {
+            std::sqrt(x.L2 * x.L2 + y.L2 * y.L2 + z.L2 * z.L2),
+            std::sqrt(x.H1 * x.H1 + y.H1 * y.H1 + z.H1 * z.H1),
+        };
+    }
+};
+
+struct vector_result_info {
+    vector_norm norm;
+    vector_norm error;
+
+    auto relative_error() const -> scalar_norm {
+        auto const total_norm = norm.total();
+        auto const total_error = error.total();
+        return {
+            total_error.L2 / total_norm.L2,
+            total_error.H1 / total_norm.H1,
+        };
+    }
+};
+
+struct maxwell_result_info {
+    vector_result_info E;
+    vector_result_info H;
+};
+
+auto print_result_info(maxwell_result_info const& res) -> void;
+
+#endif  // MAXWELL_RESULTS_HPP
