645b62de45077c21cc73ddab2d7be7211ae74fe4
Marcin Łoś <marcin.los.91@gmail.com>
2021-08-29 19:30:12 +0200

    Add quotes around script names in run-checks.sh

--- scripts/run-checks.sh
+++ scripts/run-checks.sh
@@ -2,7 +2,7 @@
 
 DIR=$(realpath "$(dirname "${BASH_SOURCE[0]}")")
 
-${DIR}/spelling.sh
-${DIR}/copyright.py check
-${DIR}/guards.py check
-${DIR}/format.py check
+"${DIR}/spelling.sh"
+"${DIR}/copyright.py" check
+"${DIR}/guards.py" check
+"${DIR}/format.py" check
