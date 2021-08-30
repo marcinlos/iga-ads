90058b018057855887c808f376456f48a6f6be74
Marcin Łoś <marcin.los.91@gmail.com>
2021-08-30 18:26:10 +0200

    Double quote variables in Coverity script

--- scripts/coverity-submit.sh
+++ scripts/coverity-submit.sh
@@ -7,9 +7,9 @@ FILE="scan-data.tgz"
 tar czf "${FILE}" cov-int
 
 curl \
-    --form token=${TOKEN} \
+    --form token="${TOKEN}" \
     --form email=marcin.los.91@gmail.com \
-    --form file=@${FILE} \
-    --form version=${GITHUB_SHA} \
+    --form file=@"${FILE}" \
+    --form version="${GITHUB_SHA}" \
     --form description="develop build" \
     https://scan.coverity.com/builds?project=marcinlos%2Figa-ads
