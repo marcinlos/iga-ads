7327de462e5791750f139b9e64f0e30bdc45d0b4
Marcin Łoś <marcin.los.91@gmail.com>
2023-07-04 20:51:49 +0200

    Add auxiliary product space factory

---
+++ include/ads/experimental/space_factory.hpp
@@ -0,0 +1,30 @@
+// SPDX-FileCopyrightText: 2015 - 2021 Marcin Łoś <marcin.los.91@gmail.com>
+// SPDX-License-Identifier: MIT
+
+#ifndef ADS_EXPERIMENTAL_SPACE_FACTORY_HPP
+#define ADS_EXPERIMENTAL_SPACE_FACTORY_HPP
+
+#include <utility>
+
+#include "ads/experimental/all.hpp"
+
+namespace ads {
+
+class space_factory {
+private:
+    ads::global_dof offset_ = 0;
+
+public:
+    template <typename Space, typename... Args>
+    auto next(Args&&... args) -> Space {
+        auto space = Space{std::forward<Args>(args)..., offset_};
+        offset_ += space.dof_count();
+        return space;
+    }
+
+    auto dim() const -> ads::global_dof { return offset_; };
+};
+
+}  // namespace ads
+
+#endif  // ADS_EXPERIMENTAL_SPACE_FACTORY_HPP
