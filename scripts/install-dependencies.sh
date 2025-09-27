f544e5298d6ec0b7c29cf96d42cb1bf4d398d671
Marcin Łoś <marcin.los.91@gmail.com>
2025-09-27 05:01:11 +0200

    Patch Galois for gcc >= 11

--- scripts/install-dependencies.sh
+++ scripts/install-dependencies.sh
@@ -80,6 +80,8 @@ cmake --install Lyra/build
 GALOIS_VER=6.0
 git clone --branch release-${GALOIS_VER} --depth=1 --quiet https://github.com/IntelligentSoftwareSystems/Galois
 
+sed -i '36i #include <optional>' Galois/tools/graph-convert/graph-convert.cpp
+
 mkdir -p Galois/build
 
 cmake \
