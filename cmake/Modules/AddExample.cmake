10492f33ce6d7d6775c94eaed9b388e8e4dc3775
Marcin Łoś <marcin.los.91@gmail.com>
2026-04-10 20:15:42 +0200

    Move example definitions to separate files

---
+++ cmake/Modules/AddExample.cmake
@@ -0,0 +1,43 @@
+include_guard(GLOBAL)
+
+# Helper function for defining example applications
+function(add_example name)
+    set(optional_deps MUMPS GALOIS)
+    set(single_value_args "")
+    set(multi_value_args LIBS SRC)
+
+    cmake_parse_arguments(
+        PROGRAM
+        "${optional_deps}"
+        "${single_value_args}"
+        "${multi_value_args}"
+        ${ARGN}
+    )
+
+    set(_target_name "ads.example.${name}")
+    set(_define_target TRUE)
+
+    foreach(_dep IN LISTS optional_deps)
+        if(PROGRAM_${_dep} AND NOT ADS_USE_${_dep})
+            set(_define_target FALSE)
+        endif()
+    endforeach()
+
+    if(_define_target)
+        add_executable(${_target_name} ${PROGRAM_SRC})
+        target_link_libraries(
+            ${_target_name}
+            PRIVATE #
+                ADS::ADS
+                ads-options-private
+                ${PROGRAM_LIBS}
+        )
+        set_target_properties(
+            ${_target_name}
+            PROPERTIES #
+                OUTPUT_NAME ${name}
+                RUNTIME_OUTPUT_DIRECTORY "${PROJECT_BINARY_DIR}/bin"
+        )
+        add_dependencies(ads.examples ${_target_name})
+    endif()
+endfunction()
