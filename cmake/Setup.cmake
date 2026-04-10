745cda700c0d20a02da5834087e1b13658e05197
Marcin Łoś <marcin.los.91@gmail.com>
2026-04-10 13:18:11 +0200

    Do not set default build type
    This is usually not recommended.

--- cmake/Setup.cmake
+++ cmake/Setup.cmake
@@ -5,12 +5,6 @@ set(CMAKE_CXX_EXTENSIONS OFF)
 
 list(APPEND CMAKE_MODULE_PATH "${PROJECT_SOURCE_DIR}/cmake/Modules/")
 
-# Default to Release build
-if (NOT CMAKE_BUILD_TYPE AND NOT GENERATOR_IS_MULTI_CONFIG)
-  message(STATUS "No build type selected, default to Release")
-  set(CMAKE_BUILD_TYPE "Release")
-endif()
-
 # Imaginary libraries to propagate settings
 add_library(ads-options-public INTERFACE)
 add_library(ads-options-private INTERFACE)
